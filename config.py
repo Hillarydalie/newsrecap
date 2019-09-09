4314a7fca6f042023a1edb3d1f9586a28fbfbe45
hillarydalie <hidalie@gmail.com>
2019-09-09 10:24:20 +0300

    Add config functions

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+  """
+  Main parent config class
+  """
+  NEWS_URL = 'https://newsapi.org/v2/sources?apiKey='
+  API_KEY = os.environ.get('API_KEY')
+  ARTICLE_URL = 'https://newsapi.org/v2/everything?q={}&sortBy=publishedAt&apiKey='
+  SOURCE_HEADLINES = 'https://newsapi.org/v2/everything?sources={}&apiKey='
+
+
+class Devconfig(Config):
+  """
+  Development configuration child class
+  """
+
+  DEBUG = True
+
+class Prodconfig(Config):
+  """
+  Production configuration child class
+  """
+  pass
+
+configurations = {
+  'development' : Devconfig, 'production' : Prodconfig
+}
